0a1b292575d5f7f3417275ee87fc2e19b2769f03
leetwinoff <leetwinoff92@gmail.com>
2023-05-30 19:29:59 -0500

    Added UserModel, made migrations

--- user_services/models.py
+++ user_services/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+
+
