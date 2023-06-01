c6287bbf6b40b66bf6b7d6668cf0b6fcfb04e34d
leetwinoff <leetwinoff92@gmail.com>
2023-06-01 12:10:17 -0500

    Added BorrowingViewSet

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book_service.permissions import IsAdminOrReadOnly
+from borrowing_service.models import Borrowing
+from borrowing_service.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAdminOrReadOnly,)
