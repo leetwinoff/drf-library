654bfde243c2bb348985a41bbab09f97385cf152
leetwinoff <leetwinoff92@gmail.com>
2023-06-01 10:57:48 -0500

    Added Default_Permission_classes for REST_FRAMEWORK settings, to allow unauthenticated users to see list off books

--- drf_library/settings.py
+++ drf_library/settings.py
@@ -137,7 +137,11 @@ AUTH_USER_MODEL = "user_services.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
 }
 
