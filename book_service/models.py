90040f33a41b36cbdfe77ffb645f99e2201294b9
leetwinoff <leetwinoff92@gmail.com>
2023-05-31 21:24:13 -0500

    Added Book model and made migrations

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,21 @@
+from enum import Enum
+
 from django.db import models
 
-# Create your models here.
+
+class CoverChoices(Enum):
+    HARD = "HARD"
+    SOFT = "SOFT"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    covers = models.CharField(
+        max_length=4, choices=[(cover.name, cover.value) for cover in CoverChoices]
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.title
