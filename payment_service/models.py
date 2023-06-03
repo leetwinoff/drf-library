58a1a6c47128a1739bc93c3cf42c63613d016f28
leetwinoff <leetwinoff92@gmail.com>
2023-06-03 08:46:44 -0500

    Added Payment model and made migrations

--- payment_service/models.py
+++ payment_service/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from user_services.models import User
+
+
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
