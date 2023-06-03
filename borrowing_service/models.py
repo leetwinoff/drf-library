724e49c6530f9dd2a878d64c4cfa39dda05a3e94
leetwinoff <leetwinoff92@gmail.com>
2023-06-03 08:19:31 -0500

    Added ordering in Borrowing model and property "is_active" to show wich borrowing still active

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -15,8 +15,18 @@ class Borrowing(models.Model):
     book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ["actual_return_date"]
+
     def mark_as_returned(self):
         self.actual_return_date = timezone.now()
         self.save()
-        self.book.inventory += 1
-        self.book.save()
+        self.book_id.inventory += 1
+        self.book_id.save()
+        self.user_id.borrowed_books.remove(self.book_id)
+
+    @property
+    def is_active(self):
+        if self.actual_return_date is None:
+            return True
+        return False
