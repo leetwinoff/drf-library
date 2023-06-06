78db9d1a6e3ea1507306bc07d8bc6e5341c9fe32
leetwinoff <leetwinoff92@gmail.com>
2023-06-06 18:25:35 -0500

    Added Scheduled task for Celery-Beat to inform about forthcoming overdue borrowings

---
+++ borrowing_service/tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+
+from borrowing_service.telegram_helper import send_overdue_notification
+from drf_library.celery import app
+
+
+@app.task
+def send_notification():
+     return send_overdue_notification()
