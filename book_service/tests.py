b44dfc715f52eb24e11cd99881f6bba109ba4c17
leetwinoff <leetwinoff92@gmail.com>
2023-06-08 09:35:38 -0500

    Added testCase for Book model, test creation of book instance. Added TestCase for bookSerializer, tests with valid and invalid serialized data.

--- book_service/tests.py
+++ book_service/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
 
-# Create your tests here.
+from book_service.models import Book
+from book_service.serializers import BookSerializer
+
+
+class BookModelTestCase(TestCase):
+    def setUp(self):
+        self.book_data = {
+            "title": "Test Book",
+            "author": "Test Author",
+            "covers": "SOFT",
+            "inventory": 10,
+            "daily_fee": 1.99,
+        }
+
+    def test_book_creation(self):
+        book = Book.objects.create(**self.book_data)
+        self.assertEqual(book.title, self.book_data["title"])
+        self.assertEqual(book.author, self.book_data["author"])
+        self.assertEqual(book.covers, self.book_data["covers"])
+        self.assertEqual(book.inventory, self.book_data["inventory"])
+        self.assertEqual(book.daily_fee, self.book_data["daily_fee"])
+
+
+class BookSerializerTestCase(TestCase):
+    def setUp(self):
+        self.book_data = {
+            "id": 1,
+            "title": "Test Book",
+            "author": "Test Author",
+            "covers": "SOFT",
+            "inventory": 10,
+            "daily_fee": 1.99,
+        }
+        self.serializer = BookSerializer(data=self.book_data)
+
+    def test_valid_serializer(self):
+        self.assertTrue(self.serializer.is_valid())
+
+    def test_invalid_serializer(self):
+        invalid_data = {
+            "id": 1,
+            "title": "Test Book",
+            "author": "Test Author",
+            "covers": "HARD",
+            "inventory": 0,
+            "daily_fee": "invalid_fee",
+        }
+        serializer = BookSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid())
