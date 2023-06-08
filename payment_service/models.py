da6326e807a40503fd94db0b18fc14dd31f2084c
leetwinoff <leetwinoff92@gmail.com>
2023-06-08 09:11:06 -0500

    Updated payment model, added status and type choices. Made migrations

--- payment_service/models.py
+++ payment_service/models.py
@@ -4,7 +4,18 @@ from user_services.models import User
 
 
 class Payment(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ("PENDING", "Pending"),
+        ("PAID", "Paid"),
+    )
+    TYPE_CHOICES = (
+        ("PAYMENT", "Payment"),
+        ("FINE", "Fine"),
+    )
+
+    status = models.CharField(max_length=7, choices=STATUS_CHOICES, default="PENDING")
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES)
+    borrowing_id = models.IntegerField()
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
