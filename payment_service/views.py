6982765ea16845d306ebefdb68391187c0993a37
leetwinoff <leetwinoff92@gmail.com>
2023-06-09 08:16:24 -0500

    Added PaymentList and PaymentDetail views. Added render function for success and cancel url

--- payment_service/views.py
+++ payment_service/views.py
@@ -1,3 +1,34 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from book_service.permissions import IsAdminOrReadOnly
+from payment_service.models import Payment
+from payment_service.serializers import PaymentSerializer
+
+
+class PaymentListView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all()
+        return Payment.objects.filter(user=user)
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Payment.objects.all()
+
+
+def success_url(request):
+    return render(request, "success.html")
+
+
+def cancel_url(request):
+    return render(request, "cancel.html")
