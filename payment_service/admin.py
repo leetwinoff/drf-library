1b5a07955b0e4ad44e5a743ebdefeed72f4d9ba5
leetwinoff <leetwinoff92@gmail.com>
2023-06-09 08:14:04 -0500

    Registered Payment on admin site

--- payment_service/admin.py
+++ payment_service/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment_service.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "status",
+        "payment_type",
+        "borrowing_id",
+        "session_url",
+        "session_id",
+        "money_to_pay",
+    )
