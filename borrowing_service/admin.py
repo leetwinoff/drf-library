a7c5fc16765780c28f74456684557e0b1eac1a46
leetwinoff <leetwinoff92@gmail.com>
2023-06-09 08:13:49 -0500

    Registered Borrowing on admin site

--- borrowing_service/admin.py
+++ borrowing_service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from borrowing_service.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "borrow",
+        "expected_return_date",
+        "actual_return_date",
+        "book_id",
+        "user_id",
+    )
