0525963b93f9f1566b39a8ddcf99eaa1ee223a9d
leetwinoff <leetwinoff92@gmail.com>
2023-06-15 10:31:27 -0500

    Added, transfered borrow_book custom action from BookViewSet to BorrowingViewSet

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,5 +1,3 @@
-import stripe
-from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,10 +8,8 @@ from borrowing_service.models import Borrowing
 from borrowing_service.serializers import (
     BorrowingSerializer,
     ReturnBookSerializer,
+    BorrowSerializer,
 )
-from borrowing_service.telegram_helper import return_borrowing_notification
-from drf_library.settings import STRIPE_SECRET_KEY
-from payment_service.models import Payment
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
@@ -24,6 +20,8 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "return_book":
             return ReturnBookSerializer
+        if self.action == "borrow_book":
+            return BorrowSerializer
         return BorrowingSerializer
 
     def get_queryset(self):
@@ -32,72 +30,30 @@ class BorrowingViewSet(viewsets.ModelViewSet):
             return Borrowing.objects.all()
         return Borrowing.objects.filter(user_id=user)
 
+    @action(detail=False, methods=["POST"])
+    def borrow_book(self, request, pk=None):
+        serializer = BorrowSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        borrowing = serializer.save()
+
+        return Response(
+            {"detail": "Book borrowed succesfully"}, status=status.HTTP_200_OK
+        )
+
     @action(detail=True, methods=["POST"])
     def return_book(self, request, pk=None):
-        borrowing = self.get_object()
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        session_url = serializer.return_book()
 
-        if borrowing.actual_return_date is not None:
-            return Response(
-                {"detail": "Book has already been returned."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        borrowing.mark_as_returned()
-        return_borrowing_notification(borrowing)
-
-        borrowing_days = (timezone.now() - borrowing.borrow).days
-
-        borrowing_pay_amount = 0
-
-        if borrowing_days > 0:
-            daily_fee = borrowing.book_id.daily_fee
-            borrowing_pay_amount = borrowing_days * daily_fee
-        if borrowing_pay_amount == 0:
-            return Response(
-                {"detail": "Book has already been returned."}, status=status.HTTP_200_OK
-            )
-        else:
-            stripe.api_key = STRIPE_SECRET_KEY
-
-            session = stripe.checkout.Session.create(
-                payment_method_types=["card"],
-                line_items=[
-                    {
-                        "price_data": {
-                            "currency": "usd",
-                            "unit_amount": int(borrowing_pay_amount * 100),
-                            "product_data": {
-                                "name": "Borrowing Payment",
-                                "description": "Payment for overdue borrowing",
-                            },
-                        },
-                        "quantity": 1,
-                    }
-                ],
-                mode="payment",
-                success_url="http://127.0.0.1:8000/api/payment_service/payments/success/",
-                cancel_url="http://127.0.0.1:8000/api/payment_service/payments/cancel/",
-            )
-
-            payment = Payment.objects.create(
-                status="PENDING",
-                payment_type="PAYMENT",
-                borrowing_id=borrowing.id,
-                session_url=session.url,
-                session_id=session.id,
-                money_to_pay=borrowing_pay_amount,
-                user=self.request.user,
-            )
-
-            payment.borrowing = borrowing
-            payment.save()
-
-            return Response(
-                {"session_url": session.url},
-                status=status.HTTP_200_OK,
-            )
+        return Response({"detail": session_url}, status=status.HTTP_200_OK)
 
     def get_permissions(self):
-        if self.action == "return_book":
+        if self.action in ["borrow_book", "return_book"]:
             return []
         return super().get_permissions()
